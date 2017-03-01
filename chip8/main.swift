1f1840189d27c5fef77553c9fcd171667f53fb51
Arthur Dexter <arthur.dexter@livingsocial.com>
2017-03-01 11:10:41 -0600

    Initial Commit

---
+++ chip8/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  chip8
+//
+//  Created by Arthur Dexter on 3/1/17.
+//  Copyright © 2017 LivingSocial. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
